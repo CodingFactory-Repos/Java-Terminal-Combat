455b2b79e902ade3a2dbcea85019c0e3b0327f51
AxelPerrin <axel.perrin783000@gmail.com>
2021-11-17 11:01:41 +0100

    Add menu base

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("1 : Menu");
+        System.out.println("2 : Jouer");
+        System.out.println("3 : Option");
+        System.out.println("4 : Quitter");
+        Scanner sc = new Scanner(System.in);
+        String jouer = sc.nextLine();
+        int menu = sc.nextInt();
+        System.out.println(""+" " +jouer+" "+" "+" "+ menu);
+
         Player player1 = new Player("Guerrier1", 20.0, 300.0);
         System.out.println ("name :"+player1.getName());
         player1.setAttack(30);

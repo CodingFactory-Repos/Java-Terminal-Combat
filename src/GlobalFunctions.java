1c597fb396b46d384e513103ced87547b2fd2579
Louis <35641311+Loule95450@users.noreply.github.com>
2021-11-18 15:16:09 +0100

    🖼 - Move Main Menu on Function in GlobalFunctions.java

--- src/GlobalFunctions.java
+++ src/GlobalFunctions.java
@@ -73,10 +73,42 @@ public class GlobalFunctions {
     public static boolean deleteJson(String name) {
         File file = new File("./data/characters/" + name + ".json");
         
-        if(file.delete()) {
-            return true;
-        } else {
-            return false;
+        return file.delete();
+    }
+
+    public static void showMenu() {
+        // Initialize
+        Scanner scanner = new Scanner(System.in);
+        Play play = new Play();
+        Characters characters = new Characters();
+        
+        showTitle("TerminalCombat");
+
+        String[] menuArray = {"Jouer", "Caractères", "Quitter"};
+
+        for(int i=0; i<menuArray.length; i++){
+            System.out.println((i + 1) + ": " + menuArray[i]);
+        }
+
+        System.out.print("> ");
+
+        switch (scanner.nextInt()) {
+            case 1:
+                System.out.println("\n");
+                play.main(); 
+                break;
+            case 2:
+                System.out.println("\n");
+                characters.main(); 
+                break;
+            case 3:
+                System.out.print("\033[H\033[2J");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("\n");
+                System.out.println("Erreur");
+                break;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,43 +7,6 @@ class Main extends GlobalFunctions{
         System.out.print("\033[H\033[2J");
         System.out.println("Chargement...");
 
-        // Initialize
-        Scanner scanner = new Scanner(System.in);
-        Play play = new Play();
-        Characters characters = new Characters();
-
-        // Clear console output
-        System.out.print("\033[H\033[2J");
-
-        showTitle("TerminalCombat");
-
-        String[] menuArray = {"Jouer", "Caractères", "Quitter"};
-
-        for(int i=0; i<menuArray.length; i++){
-            System.out.println((i + 1) + ": " + menuArray[i]);
-        }
-
-        System.out.print("> ");
-
-        switch (scanner.nextInt()) {
-            case 1:
-                System.out.println("\n");
-                play.main(); 
-                break;
-            case 2:
-                System.out.println("\n");
-                characters.main(); 
-                break;
-            case 3:
-                System.out.print("\033[H\033[2J");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("\n");
-                System.out.println("Erreur");
-                break;
-        }
-
-    //     
+        showMenu();  
     }
 }

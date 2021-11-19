c8d5cb7b0e1994dc5f734ce4d0e57766bb36e5d1
Louis <35641311+Loule95450@users.noreply.github.com>
2021-11-19 09:19:38 +0100

    ✨ - Update class

--- src/Wizard.java
+++ src/Wizard.java
@@ -1,82 +1,36 @@
 class Wizard {
-
     private String name;
     private double health;
     private double attack;
-    private double magic;
-    private double attack_magic;
     private double initiative;
+    private double magic;
 
-    public Wizard(String name, double attack, double health, double initiative, double magic, double attack_magic) {
+    public Wizard(String name, double attack, double health, double initiative, double magic) {
         this.name = name;
         this.attack = attack;
         this.health = health;
-        this.magic = magic ;
-        this.attack_magic = attack_magic;
         this.initiative = initiative;
+        this.magic = magic * 2;
     }
 
-    public double getAttack_magic() {
-        return attack_magic;
-    }
-
-    public void setAttack_magic(double attack_magic) {
-        this.attack_magic = attack_magic;
+    public void damage(double damage) {
+        this.health -= damage;
     }
 
     public String getName() {
         return name;
     }
 
-    public void damage(double damage){
-        this.health -= damage;
-    }
-
-    public void setName(String name){
-        this.name =name;
-    }
-
-    public double getHealth(){
+    public double getHealth() {
         return health;
     }
 
-    public void setHealth (double health){
-        this.health = health;
-    }
-
-    public double getAttack () {
-        return attack;
-    }
-
-    public void setAttack (double attack) {
-        this.attack = attack;
-    }
-
-    public double getMagic() {
-        return magic;
-    }
-
-    public void setMagic(double magic) {
-        this.magic = magic;
-    }
-
-    public void decreaseMagic() {
-        magic = magic / 2;
-    }
-
-    public void AttackMagic(){
-        attack_magic = magic + attack;
+    public double getAttack() {
+        this.magic = this.magic / 2;
+        return attack + this.magic;
     }
 
     public double getInitiative() {
         return initiative;
     }
-
-
-    public void setInitiative(double initiative) {
-        this.initiative = initiative;
-    }
-
 }
-
-
